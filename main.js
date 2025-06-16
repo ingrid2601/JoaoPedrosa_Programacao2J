6bde519d38266e1ea8ecab5933fdda0a9bb3107b
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2025-06-16 13:45:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-//let usuário = "";
-
-//while (usuário === ""){
-    //usuário = prompt("Qual o seu nome ?");
-//}
-
-//let elementoSaudações = document.querySelector("#nome-usuário");
-
-//if (usuário === null){
-    //elementoSaudações.textContent = "seja muito bem vindo!"
-//}else{
-  //  elementoSaudações.textContent = usuário;
-//}
 
 function inserirNome(){
  let nomeUsuario = prompt("Qual o seu nome");
