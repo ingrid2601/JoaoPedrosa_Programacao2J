32d3d8edab2968043dc97f705b9d2e37d88b179f
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2025-06-23 11:47:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ const linguagens = ["JavaScript", "Python", "C++"]
 console.log(linguagens[0]) // “JavaScript”
 console.log(linguagens[1]) // “Python”
 console.log(linguagens[2]) // “C++”
+
+const item = document.querySelector("#lista");
+item.textContent = linguagens[0];
+
+let aluno1 = {
+  nome: "Gabriel",
+  idade: 13,
+  anoLetivo: "2ºsérie",
+  materiasFavoritas: "Geografia","Programação","Arte"
+}
+const item = document.querySelector("#aluno");
+item.textContent = aluno1.nome;
+
+tem.textContent = aluno1.nome;
+item.textContent = aluno1.idade;
+item.textContent = aluno1.materiasFavoritas[1];
